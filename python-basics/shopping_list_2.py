c526c575389bc1d767a2c4640b72e245f872c7d9
Joseph Falconer <joseph.falconer@intoglobal.com>
2017-03-10 17:38:38 +0000

    Refactored functions a little and removed commas from saved list

--- python-basics/shopping_list_2.py
+++ python-basics/shopping_list_2.py
@@ -5,17 +5,15 @@ shopping_list = []
 
 
 
-def open_list():
+def get_list():
     # check that list.txt exits
     try: 
         with open('list.txt', 'r') as l:
-            listdata = l.read().split()
-            print("Here is the list you've saved:")
-            for item in listdata:
-                print(item)
+            listdata = l.read().split()           
         l.close()
+        return listdata
     except IOError:
-        print("You haven't saved a list")
+        return None
 
 def save_list():
     pass
@@ -24,6 +22,7 @@ def save_list():
  
 def show_help():
     # print out instructions on how to use the app
+    print("")
     print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
@@ -33,18 +32,17 @@ Enter 'SHOW' to see your current list.
     
 def show_list():
     # print out the list
+    saved_list = get_list()
     print("Here's your list:")
-    
-    for item in shopping_list:
+    for item in saved_list:
         print(item)
-        
+    
 def add_to_list(new_item):
     # add new items to our list
     shopping_list.append(new_item)
     print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
-    
-open_list()
 
+show_list()    
 show_help()
 
 while True:
