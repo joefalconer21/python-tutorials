f86d1cd8646d1522ee41aa8b0e8aab42d5ebf995
Joseph Falconer <joseph.falconer@intoglobal.com>
2017-03-10 16:39:14 +0000

    Added function for opening saved list file

--- python-basics/shopping_list_2.py
+++ python-basics/shopping_list_2.py
@@ -1,6 +1,27 @@
+import io
+
 # make a list to hold onto our items
 shopping_list = []
 
+
+
+def open_list():
+    # check that list.txt exits
+    try: 
+        with open('list.txt', 'r') as l:
+            listdata = l.read().split()
+            print("Here is the list you've saved:")
+            for item in listdata:
+                print(item)
+        l.close()
+    except IOError:
+        print("You haven't saved a list")
+
+def save_list():
+    pass
+
+
+ 
 def show_help():
     # print out instructions on how to use the app
     print("What should we pick up at the store?")
@@ -22,6 +43,8 @@ def add_to_list(new_item):
     shopping_list.append(new_item)
     print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
     
+open_list()
+
 show_help()
 
 while True:
